8221aa3d3f17ea09f03847a4cf389003df17de05
Lee Jae Yeong <leey153@naver.com>
2023-10-09 13:53:52 +0900

    빈 값에 대한 예외 처리

--- ToDo_vanillaJS/src/footer/Footer.js
+++ ToDo_vanillaJS/src/footer/Footer.js
@@ -11,14 +11,15 @@ function Form({ target, onSubmit }) {
   // 만약 submit 이벤트가 발생했을때의 이벤트 리스너 등록!
   formElement.addEventListener("submit", (e) => {
     e.preventDefault()
-
-    // 예외 적용해야함
-
     // submit 시점의 input value 값을 받기위해 input을 참음
     const inputElement = document.querySelector(".form_input")
-    // 이후 value를 compoenet가 매개변수로 받아온 onSubmit 함수의 인수로 전달후 함수 호출함
-    onSubmit(inputElement.value)
-
+    // 해당 Input의 value를 따로 변수에 담음
+    const newText = inputElement.value
+    // text가 빈값 / 예외를 위한 조건
+    if (newText === 0 || newText) {
+      // 정상값이라면 component에서 전달받은 매개변수 함수를 호출하고 인수로 newText 전달
+      onSubmit(newText)
+    }
     // 적용 완료후 input창을 빈값으로 만듬
     inputElement.value = ""
   })

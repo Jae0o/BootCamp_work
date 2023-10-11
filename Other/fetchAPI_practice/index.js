f5e4d933f87b17b8f8a26d890dc93d021275f8a0
Lee Jae Yeong <leey153@naver.com>
2023-10-11 14:48:00 +0900

    더미데이터를 이용한 option 만들기와 이벤트 등록 ✏️

--- Other/fetchAPI_practice/index.js
+++ Other/fetchAPI_practice/index.js
@@ -2,21 +2,25 @@ import ProductOptions from "./src/ProductOptions.js"
 
 const testData = [
   {
+    id: 1,
     optionName: "test1",
     optionPrice: 1000,
     stock: 40
   },
   {
+    id: 2,
     optionName: "test2",
     optionPrice: 2000,
     stock: 0
   },
   {
+    id: 3,
     optionName: "test4",
     optionPrice: 4000,
     stock: 5
   },
   {
+    id: 4,
     optionName: "test3",
     optionPrice: 3000,
     stock: 27
@@ -24,4 +28,8 @@ const testData = [
 ]
 
 const appElement = document.querySelector('#app');
-new ProductOptions({ target: appElement, state: testData })
+new ProductOptions({
+  target: appElement,
+  state: testData,
+  onSelect: (params) => console.log(params)
+})
--- Other/fetchAPI_practice/src/ProductOptions.js
+++ Other/fetchAPI_practice/src/ProductOptions.js
@@ -1,7 +1,7 @@
 export default function ProductOptions({ target, state, onSelect }) {
   const selectElement = document.createElement('select');
   target.appendChild(selectElement)
-  console.log(target)
+
   // 재고를 조회하는 API
   // 옵션을 조회하는 API 가 다르게 존재함
   // 상품명 + 옵션명 + 재고 N개 을 같이 보여줘야함
@@ -9,6 +9,7 @@ export default function ProductOptions({ target, state, onSelect }) {
 
   // [
   //  { 
+  //    id : n,
   //    optionName : "",
   //    optionPrice : n,
   //    stock : n  
@@ -23,17 +24,29 @@ export default function ProductOptions({ target, state, onSelect }) {
     this.render()
   }
 
+  const createOptionFullName = ({ optionName, optionPrice, stock }) => {
+    return `${optionName} ${optionPrice > 0 ? `(옵션가 ${optionPrice}))` : ""} | ${stock > 0 ? `재고 : ${stock}` : `재고 없음`}`
+  }
+
+  selectElement.addEventListener('change', (e) => {
+    const optionID = parseInt(e.target.value)
+
+    const option = this.state.find(option => option.id === optionID)
+    onSelect(option)
+  })
+
   this.render = () => {
 
     if (this.state && Array.isArray(this.state)) {
       selectElement.innerHTML = `
+      <option> 선택하기 </option>
       ${this.state.map(option =>
-        `<option value="${option.id}">${option.optionName}</option>`
+        `<option ${option.stock === 0 ? "disabled" : ""} value="${option.id}">${createOptionFullName(option)}</option>`
       ).join("")}
       `
-      console.log(state)
     }
   }
+
 
   this.render()
 }

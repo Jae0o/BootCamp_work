98d7c371fea6c2ffb52f94fcc8c8990525a6f2f6
Lee Jae Yeong <leey153@naver.com>
2023-10-14 19:01:03 +0900

    코딩 테스트 문제 푸는중... 실풰

--- Test/CodingTest/index.js
+++ Test/CodingTest/index.js
@@ -1,87 +1,36 @@
-"use strict"
+"use strict";
 
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n')
-const index = +input[1]
-input = [...input[0]]
-console.log(input[index - 1])
+const [N, M] = input.shift()
+const graph = Array.from({ length: +N + 1 }, () => [])
 
+for (const [start, _, end] of input) {
+  graph[+start].push(+end)
+  graph[+end].push(+start)
+}
 
+const check = Array.from({ length: +N + 1 }, () => true)
+check[0] = false
+let count = 0;
 
+for (let i = 0; i <= N; i += 1) {
 
+  if (check[i]) {
+    count++
+    const stack = [i]
+    check[i] = false
 
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-// class Queue {
-//   constructor() {
-//     this.front = null;
-//     this.tail = null;
-//     this.size = 0;
-//   }
-//   push(value) {
-//     const newNode = new Node(value)
+    while (stack.length !== 0) {
+      const value = stack[stack.length - 1]
+      if (!check[value] && graph[value].length !== 0) {
+        stack.push(graph[value].pop())
+      } else {
+        check[stack.pop()] = false
+      }
+    }
+  }
+}
 
-//     if (this.front === null) {
-//       this.front = newNode;
-//       this.tail = newNode;
-//     } else {
-//       this.tail.next = newNode;
-//       this.tail = newNode;
-//     }
-//     this.size++
-//   }
-//   getFront() {
-//     if (this.front === null) {
-//       return -1
-//     }
-//     return this.front.value
-//   }
-//   getTail() {
-//     if (this.tail === null) {
-//       return -1
-//     }
-//     return this.tail.value
-//   }
-//   size() {
-//     return this.size
-//   }
-//   shift() {
-//     if (this.front === null) {
-//       return -1
-//     }
-//     const value = this.front.value;
-//     this.front = this.front.next;
-//     this.size--
-//     if (this.front === this.tail) {
-//       this.tail = null
-//     }
-//     return value
-//   }
-// }
-// const queue = new Queue();
-// for (let i = 1; i <= input[0]; i++) {
-//   const [order, value] = input[i].split(" ")
-//   if (order === "push") {
-//     queue.push(value)
-//   } else if (order === "front") {
-//     console.log(queue.getFront())
-//   } else if (order === "pop") {
-//     console.log(queue.shift())
-//   } else if (order === "size") {
-//     console.log(queue.size)
-//   } else if (order === "back") {
-//     console.log(queue.getTail())
-//   } else if (order === "empty") {
-//     if (queue.size === 0) {
-//       console.log(1)
-//     } else {
-//       console.log(0)
-//     }
-//   }
-// }
-// console.log(queue.size)
+console.log(count)
--- Test/index.js
+++ Test/index.js
@@ -1,4 +1,100 @@
 "use strict"
 
 
-console.log(document.querySelector('body'))
+
+
+
+function setItem(text) {
+  const request = indexedDB.open("TestIndexDB")
+
+  request.addEventListener('success', function (e) {
+    const DB = e.target.result;
+
+    const store = DB.transaction("test", "readwrite").objectStore("test")
+
+    store.add({
+      text
+    })
+
+    store.add({
+      text: text + 1
+    })
+  })
+  request.addEventListener("upgradeneeded", function (e) {
+    const DB = e.target.result;
+    DB.createObjectStore("test", { keyPath: "id", autoIncrement: true })
+  })
+  request.addEventListener('error', (e) => { console.log(e) })
+
+}
+
+
+// setItem("Testcodefdsfds")
+
+
+function getItem(id) {
+  const request = indexedDB.open("TestIndexDB", 3)
+
+  request.addEventListener('success', function (e) {
+    const DB = e.target.result;
+    const store = DB.transaction("test", "readonly").objectStore("test")
+
+    const valueRequest = store.get(id)
+    valueRequest.addEventListener("success", (e) => console.log(e.target.result))
+  })
+  request.addEventListener("upgradeneeded", function (e) {
+    const DB = e.target.result;
+    DB.createObjectStore("test", { keyPath: "id", autoIncrement: true })
+  })
+  request.addEventListener('error', (e) => { console.log(e) })
+}
+
+
+// getItem(3)
+
+
+function getAll() {
+  const request = indexedDB.open("TestIndexDB", 3)
+
+  request.addEventListener('success', function (e) {
+    const DB = e.target.result;
+    const store = DB.transaction("test", "readonly").objectStore("test")
+
+    const valueRequest = store.getAll()
+    valueRequest.addEventListener("success", (e) => console.log(e.target.result))
+  })
+  request.addEventListener("upgradeneeded", function (e) {
+    const DB = e.target.result;
+    DB.createObjectStore("test", { keyPath: "id", autoIncrement: true })
+  })
+  request.addEventListener('error', (e) => { console.log(e) })
+}
+
+// getAll()
+
+
+function getRemove(id) {
+  const request = indexedDB.open("TestIndexDB", 3)
+
+  request.addEventListener('success', function (e) {
+    const DB = e.target.result;
+    const store = DB.transaction("test", "readwrite").objectStore("test")
+
+    const valueRequest = store.delete(id)
+    valueRequest.addEventListener("success", (e) => console.log(e.target.result))
+  })
+  request.addEventListener("upgradeneeded", function (e) {
+    const DB = e.target.result;
+    DB.createObjectStore("test", { keyPath: "id", autoIncrement: true })
+  })
+  request.addEventListener('error', (e) => { console.log(e) })
+}
+
+
+
+
+
+function index() {
+
+  const request = indexedDb.op
+}

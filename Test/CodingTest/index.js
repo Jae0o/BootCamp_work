e651fee9ad8f06207178e2aca63f819b5ce147be
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-11-20 15:57:35 +0900

    Vue createApp Instance

--- Test/CodingTest/index.js
+++ Test/CodingTest/index.js
@@ -1,14 +1,9 @@
 "use strict";
 
-// const fs = require("fs");
-// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-// let input = fs.readFileSync(filePath).toString().trim().split("\n");
-// const [A, B] = [...input[0]].reverse().join("").split(" ");
-// +A > +B ? console.log(A) : console.log(B);
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const URLReg =
-  /(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-
-const emailReg = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/g;
-
-console.log("leey153@naver.com".match(emailReg));
+for (const item of input) {
+  console.log(parseInt(item[0]) + parseInt(item[2]));
+}

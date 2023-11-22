fa46b6042b14a8354e4b2059a16a6cb26fd36368
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-11-22 17:01:01 +0900

    [Vue] vue 초기 설치와 parcel

---
+++ Vue/11.parcel_install_vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hi! first install Vue Package",
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
---
+++ Vue/11.parcel_install_vue/src/index.js
@@ -0,0 +1,5 @@
+import * as Vue from "vue";
+import App from "./App.vue";
+
+const app = Vue.createApp(App);
+const vm = app.mount("#app");

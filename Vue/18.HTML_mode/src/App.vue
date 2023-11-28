4085d248f262c7382267773fb981eba9494b5cca
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-11-28 11:34:45 +0900

    [Vue] HTML mode

---
+++ Vue/18.HTML_mode/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <RouterLink to="/">home</RouterLink>
+  <RouterLink to="/about">about</RouterLink>
+  <RouterView />
+  <button @click="$router.push(`/`)">Home</button>
+  <button @click="$router.push(`/about`)">About</button>
+</template>
+
+<script></script>

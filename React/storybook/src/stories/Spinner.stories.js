8e8cde5c238ac89f76c82664ba379ee27fd7275a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-07 15:58:31 +0900

    [StoryBook] Spinner

---
+++ React/storybook/src/stories/Spinner.stories.js
@@ -0,0 +1,15 @@
+import Spinner from "../components/Spinner";
+
+export default {
+  title: "Component/Spinner",
+  component: Spinner,
+  argTypes: {
+    size: { defaultValue: 24, control: { type: "number" } },
+    color: { control: "color" },
+    loading: { control: "boolean" },
+  },
+};
+
+export const Default = (args) => {
+  return <Spinner {...args} />;
+};

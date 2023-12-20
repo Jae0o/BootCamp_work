e9f54a5ba0e2d786e6a0c8477a801743eac9e87a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-20 14:24:51 +0900

    [Test공간]

---
+++ testbox/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="test__container">
+      <button className="test__button">Test용 버튼!</button>
+    </main>
+  );
+}
+
+export default App;
